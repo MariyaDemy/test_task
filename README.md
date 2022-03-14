b68e91159ea7e93c5c55273dc79c6e87d298ab3f
MariyaDemy <Mariya.Demi373@yandex.ru>
2022-03-14 17:13:34 +0300

    fix task 3,4

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # test_task
+
+Вёрстка секции https://mariyademy.github.io/test_task/dist/
+●	Требования:
+●	Адаптивная вёрстка (резиновая)
+●	Семантическая вёрстка 
+●	Препроцессор – SCSS 
+●	Методология – BEM 
+●	Pixel perfect (3-5% допустимое расхождение)
+●	Browser support - Safari, Chrome, Firefox 
+
++ 4 задачи по JavaScript (dist/tasks.js)
--- dist/tasks.js
+++ dist/tasks.js
@@ -2,11 +2,11 @@
 
 const createSecretHolder = function (secret) {
     return {
-        getSecret: function() {
+        getSecret: function () {
             return secret;
         },
 
-        setSecret: function(newSecret) {
+        setSecret: function (newSecret) {
             return secret = newSecret;
         }
     }
@@ -22,27 +22,44 @@ console.log(obj.getSecret())
 let initialArr = [7, 6, 15, 8, 12, 10];
 
 const bitsSort = function (arr) {
-    return arr.sort((a,b) => a.toString(2).replace(/0/g,"").length - b.toString(2)
-    .replace(/0/g,"").length || a - b);
+    return arr.sort((a, b) => a.toString(2).replace(/0/g, "").length - b.toString(2)
+        .replace(/0/g, "").length || a - b);
 }
 
 console.log(bitsSort(initialArr));
 
+// task 3
+
+// Вам дан объект содержащий языки и оценки по этим языкам.
+// Необходимо вернуть массив языков где оценки => 60, отсортированный в убывающем порядке по оценкам.
+// {"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
+// {"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
+// {"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
+
+function myLanguages(results) {
+
+    return Object.keys(results).filter(res => results[res] >= 60).sort((a, b) => results[b] - results[a])
+
+}
+
 // task 4
 
+// for (var i = 0; i < 5; i++) {
+//     setTimeout(() => {
+//     console.log(i);
+//     }, 1000);
+//     }
+
+// output: 5 5 5 5 5
+
+
 for (var i = 0; i < 5; i++) {
-    setTimeout(function(i) {
-      console.log(i);
-    }, 1000, i);
-  }
+    function foo(number) {
+        setTimeout(() => {
+            console.log(number);
+        }, 1000)
+    }
+    foo(i);
+}
 
-
-  
-
-
-
-
-
-
-
-
+//output: 0 1 2 3 4
